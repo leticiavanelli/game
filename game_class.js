a535b11e5b0189dd528136a69b53fe7f6cd9cfdf
Letícia <leticia_vanelli@estudante.sesisenai.org.br>
2024-03-13 09:00:44 -0300

    criando as classes

---
+++ game_class.js
@@ -0,0 +1,73 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+    des_obj(){
+        des.fillStyle = this.a
+        des.fillRect(this.x,this.y,this.w,this.h,this.a)
+
+    }
+    des_img(){
+       des.drawImage(img,this.x, this.y,this.w,this.h)
+         let img = new Image()
+        img.src = this.a
+    }
+}
+
+class Boneco extends Obj{
+    dir = 0
+    pts = 0
+    vida = 5
+    frame = 1
+    tempo = 0
+
+    des_boneco(){
+
+    }
+
+    atualiza_boneco(){
+
+    }
+
+    point(){
+
+    }
+    
+    colid(){
+
+    }
+}
+
+class Veneno extends Obj{
+    atualiza_inim(){
+
+    }
+
+    recomeca(){
+
+    }
+}
+
+class Estrada extends Obj{
+    des_estrada(){
+
+    }
+}
+class Fruta extends Obj{
+    atualiza_fruta(){
+
+    }
+    recomeca(){
+        
+    }
+
+}
+class Text{
+    des_text(){
+
+    }
+}
---
+++ game_main.js
@@ -0,0 +1,4 @@
+let des = document.getElementById('des').getContext('2d')
+
+
+let boneco = new Boneco(100,400,160,160,'./assests/boneco.jpg')
